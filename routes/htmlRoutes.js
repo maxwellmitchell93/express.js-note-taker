17f9dc8728c4f36527dadc17145153b75b6fa640
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-16 18:18:12 -0700

    html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,15 @@
+// create a new router object
+const htmlRoutes = require('express').Router();
+const path = require('path');
+
+// GET '/notes' to return notes.html.
+htmlRoutes.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+// GET '*' (any routes) to return index.html.
+htmlRoutes.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+})
+
+module.exports = htmlRoutes;
