2229ab4ff21e02d66419cc67ef561dc526908989
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-16 22:12:23 -0700

    added html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,12 +4,12 @@ const path = require('path');
 
 // GET '/notes' to return notes.html.
 htmlRoutes.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/notes.html'));
 });
 
 // GET '*' (any routes) to return index.html.
 htmlRoutes.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/assets/index.html'));
 })
 
 module.exports = htmlRoutes;
