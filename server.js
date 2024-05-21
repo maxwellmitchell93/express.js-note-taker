c764b945183647d01b00d1c143c77c1033cbaa4a
Maxwell Mitchell <155778466+maxwellmitchell93@users.noreply.github.com>
2024-05-20 19:02:50 -0700

    Express dependencies. Added Middleware. Added HTML and API routes. App listener.

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// import express dependency
+// Import express dependency
 const express = require('express');
 
-// assign express() function to a new variable name: app
-const app = express();
-const PORT = process.env.PORT || 3001;
-
-// middleware to make 'public' folder static and accessible to front-end users
+// Middleware for publuc folder
 app.use(express.static('public'));
 
-// middleware for parsing application/json and urlencoded data
+// Middleware for parsing json 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// import HTML and API routes 
+// Assign express() function to a new variable name: app
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// HTML and API routes 
 const apiRouter = require('./routes/apiRoutes');
 const htmlRouter = require('./routes/htmlRoutes');
 app.use('/api', apiRouter);
